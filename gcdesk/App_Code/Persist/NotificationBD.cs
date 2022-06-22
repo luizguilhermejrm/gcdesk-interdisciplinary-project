607e785f9a4c21df4a270e79038be426e51a045b
LuizRomao02 <luizromao7@outlook.com>
2022-06-21 21:05:36 -0300

    feat: finished notification status ticket colaborator

--- gcdesk/App_Code/Persist/NotificationBD.cs
+++ gcdesk/App_Code/Persist/NotificationBD.cs
@@ -16,7 +16,7 @@ public class NotificationBD
         IDbCommand objCommand;
         IDataAdapter objAdapter;
         objConection = Mapped.Connection();
-        string sql = "SELECT * FROM notification A INNER JOIN ticket B ON A.tic_id = B.tic_id WHERE user_id =?cod";
+        string sql = "SELECT * FROM notification A INNER JOIN ticket B ON A.tic_id = B.tic_id WHERE user_id =?cod ORDER BY not_id DESC";
         objCommand = Mapped.Command(sql, objConection);
         objAdapter = Mapped.Adapter(objCommand);
         objCommand.Parameters.Add(Mapped.Parameter("?cod", id));
@@ -61,29 +61,54 @@ public class NotificationBD
         }
 
     }
+    public String SelectIdTicket()
+    {
+        Ticket obj = new Ticket();
+
+        System.Data.IDbConnection objConection;
+        System.Data.IDbCommand objCommand;
+        System.Data.IDataReader objDataReader;
+        objConection = Mapped.Connection();
+        objCommand = Mapped.Command("SELECT MAX(tic_id) ULTIMO_ID FROM ticket", objConection);
+        objDataReader = objCommand.ExecuteReader();
+        while (objDataReader.Read())
+        {
+            obj.TicketId = Convert.ToInt32(objDataReader["ULTIMO_ID"]);
+
+        }
+        string resultado = Convert.ToString(obj.TicketId);
+        objDataReader.Close();
+        objConection.Close();
+        objCommand.Dispose();
+        objConection.Dispose();
+        objDataReader.Dispose();
+        return resultado;
+    }
 
     /// <summary>
     /// 
     /// --> Metodo sendo utilizado nat <--
     /// 
     /// </summary>
-    /// <param name="idTicket"></param>
     /// <param name="timeMessage"></param>
     /// <returns>Insere no banco um novo status de Notificacao para o chamado</returns>
-    public int InsertNotificationStatusCreate(int idTicket, string timeMessage)
+    public int InsertNotificationStatusCreate(string timeMessage)
     {
         try
         {
+            string idTicket = SelectIdTicket();
+
             IDbConnection dbConnection;
             IDbCommand dbCommand;
-            string sql = @"INSERT INTO notification (not_description, not_title, tic_id, not_timeMensage) 
+            string sql = @"INSERT INTO notification (not_description, not_title, tic_id, not_timeMensage, not_status) 
                         VALUES ('O seu chamado foi criado com sucesso.',
-                                'Chamado Criado', ?tic_id, ?timeMessage);";
+                                'Chamado Criado', ?tic_id, ?timeMessage, 0)";
 
             dbConnection = Mapped.Connection();
             dbCommand = Mapped.Command(sql, dbConnection);
-            dbCommand.Parameters.Add(Mapped.Parameter("?timeMessage", timeMessage));
             dbCommand.Parameters.Add(Mapped.Parameter("?tic_id", idTicket));
+            dbCommand.Parameters.Add(Mapped.Parameter("?timeMessage", timeMessage));
+
             dbCommand.ExecuteNonQuery();
             dbConnection.Close();
             dbCommand.Dispose();
@@ -96,4 +121,7 @@ public class NotificationBD
         }
 
     }
+
+
+
 }
